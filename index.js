97dcf216df65983878ed38556fa7ce712b986055
AndressaGomes872 <andressagomes.bg217@academico.ifsul.edu.br>
2021-04-08 22:02:24 -0300

    usando ejs nas paginas

--- index.js
+++ index.js
@@ -14,9 +14,17 @@ app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',function(req,res){
-res.send("Olá Andressa")
+res.render('index.ejs',{})
 })
 
+app.get('/usuarios',function(req,res){
+    res.render('usuarios.ejs',{ usuarios:[
+    {nome:'Andressa',email:'andressa.jj.55@gmail.com'},
+    {nome:'Maria',email:'maria@gmail.com'},
+    {nome:'Tereza',email:'tereza@gmail.com'},
+    {nome:'Ana',email:'ana@gmail.com'}, 
+    ]})
+})
 app.listen(3000,function(){
     console.log("Conexão Inicializada")
 })
